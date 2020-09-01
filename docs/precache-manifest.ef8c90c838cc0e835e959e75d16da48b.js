a9d910c2450a78a7d2b7571bfa46bea6eee97cd7
Thomas SCHWEBEL <contact@thomas-schwebel.fr>
2020-09-01 15:51:19 +0200

    mv build -> docs for simplicity of use with GH pages

---
+++ docs/precache-manifest.ef8c90c838cc0e835e959e75d16da48b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f0fb8c584cc12beb46609dfb73b56f94",
+    "url": "./index.html"
+  },
+  {
+    "revision": "239ee1315448d86d791c",
+    "url": "./static/css/main.411da329.chunk.css"
+  },
+  {
+    "revision": "43b2f8d6fe1efcd40a0b",
+    "url": "./static/js/2.d2f48a8b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "./static/js/2.d2f48a8b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "239ee1315448d86d791c",
+    "url": "./static/js/main.39c53c64.chunk.js"
+  },
+  {
+    "revision": "118be067b7bdfa204218",
+    "url": "./static/js/runtime-main.43dc47e2.js"
+  }
+]);
