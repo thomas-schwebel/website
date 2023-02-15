b6944731a375ba52ee24ff0c27a8b6b9b3480de2
Thomas Schwebel <contact@thomas-schwebel.fr>
2023-02-15 11:46:07 +0100

    add adevinta contract duration

--- src/pages/resume/index.js
+++ src/pages/resume/index.js
@@ -48,7 +48,7 @@ function Resume() {
                     title="Software Engineer"
                     from="Jan 21"
                     to="now">
-                        <p>Platform Teams @ Adevinta<br/>
+                        <p>Platform Teams @ Adevinta (2 years contract)<br/>
                         - Java 9/11/17, Spring boot, Google Cloud, Helm, Kubernetes<br/>
                         - Building a local SAML identity provider based on OAuth + OpenID Connect<br/>
                         - Identity migrations from Keycloak, Auth0<br/>
